471d7d8ba0ffd0ae95f4bf699c041b2bc8f8cb92
eugeniaguibernau <eugenia.guibernau@correo.ucu.edu.uy>
2024-10-09 16:01:59 -0300

    carpetas

---
+++ src/Library/EnumTipos.cs
@@ -0,0 +1,22 @@
+namespace Library;
+
+public enum Tipos
+{
+    FUEGO,
+    PLANTA,
+    AGUA,
+    PSIQUICO,
+    ELECTRICO,
+    BICHO,
+    HADA,
+    VENENO,
+    NORMAL,
+    ROCA,
+    TIERRA,
+    HIELO,
+    SINIESTRO,
+    FANTASMA,
+    LUCHA,
+    DRAGON,
+    VOLADOR,
+    }
