070c23b065ae6e9207cf12a141d59ce5feca2b17
eugeniaguibernau <eugenia.guibernau@correo.ucu.edu.uy>
2024-10-10 14:24:07 -0300

    Entrenador casi terminado

--- src/Library/Logica/Entrenador.cs
+++ src/Library/Logica/Entrenador.cs
@@ -1,6 +1,53 @@
+using System.Text;
+using Library.Interfaces;
+
 namespace Library.Logica;
 
 public class Entrenador
 {
+    private string nombre;
+    private List<IPokemon> pokemons;
+    private IPokemon pokemonActivo;
+    private int contadorEspecial;
+
+    private List<IMovimiento> ataquesDisponibles;
+    //faltan getters y setters y constructor
     
+    
+    
+    public string ObtenerStatusPokemon()
+    {
+        StringBuilder statusBuilder = new StringBuilder();       
+        foreach (var pokemon in pokemons)
+        {
+            statusBuilder.AppendLine($"{pokemon.Nombre}: {pokemon.SaludActual}/{pokemon.SaludTotal} HP");
+        }
+        
+        return statusBuilder.ToString();
+    }
+    public bool TienePokemonsRestantes()
+    {
+        foreach (var poke in pokemons)
+        {
+            if (!poke.Debil)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool PuedeUsarAtaqueEspecial()
+    {
+        if (contadorEspecial == 0)
+        {
+            return true;
+        }
+    
+        return false; 
+    }
+
+    public List<IMovimiento> VerAtaquesDisponibles()
+    {
+        return this.ataquesDisponibles;
+    }
 }
