c73eec751b300c9efc5f08c624be89910475aff6
Mateo Di Fortuna <mdifortuna@datanomik.com>
2024-10-10 10:40:38 -0300

    ..

--- src/Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ src/Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8721b9b94ffa6866b249a00ac78e2549a94b9edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd0d44db8811c519e25102007cdf4afa77ba0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
